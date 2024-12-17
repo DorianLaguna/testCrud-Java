92ba5038623eb4763ceb86e4e9d47381112cd86d
DorianLaguna <richardlgcs@gmail.com>
2024-12-16 21:14:44 -0600

    project created

--- src/test/java/com/crud/testcrud/TestCrudApplicationTests.java
+++ src/test/java/com/crud/testcrud/TestCrudApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class TestCrudApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
